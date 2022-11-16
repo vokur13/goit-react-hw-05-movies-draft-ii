34959dc30f53e8c4bb71952f66ab96b597ce328c
Volodymyr Kurov <vokur13@gmail.com>
2022-11-16 14:45:26 +0200

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -13,5 +13,10 @@ export const getMovie = async () => {
   return data;
 };
 
+export const getMovieByID = async () => {
+  const { data } = await axios.get(`/trending/movie/day?${searchParams}`);
+  return data;
+};
+
 // const api_movie =
 //   'https://api.themoviedb.org/3/trending/movie/day?api_key=e1d2d59faab8416a91a95646b10aa32e';
